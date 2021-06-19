095dade1b8eca251865d80041d1eadb5c1d0b981
Vishal Sharma <vishal.sharma1@mail.vinove.com>
2021-06-19 11:28:16 +0530

    fastify schema for object

---
+++ controller/items.js
@@ -0,0 +1,14 @@
+const data = require('../data')
+
+const getData = (rea, res) =>{
+    res.send(item)
+}
+const getItem = (req, res) =>{
+    const {id} = req.params
+    const item = data.find((ele) => ele.id == id)
+    res.send(item)
+}
+module.exports = {
+    getData,
+    getItem
+}
--- routes/items.js
+++ routes/items.js
@@ -1,19 +1,28 @@
 const data = require('../data')
+const {getData, getItem} = '../controller/items'
+
+//item schema
+
+const Item =  {
+    type : 'object',
+    properties : {
+        id : {type : 'string'},
+        name : {type : 'string'}
+    }
+}
+
 //for array data
 const getDataOpts = {
     schema : {
         response : {
             200 : {
                 type :  'array',
-                items : {
-                    type : 'object',
-                    properties : {
-                        id : {type : 'string'},
-                        name : {type : 'string'}
-                    }
-                }
+                items : Item
             }
         }
+    },
+    handler : (req, res) => {
+        res.send( data)
     }
 }
 //for object
@@ -21,27 +30,32 @@ const getDataOpts = {
 const getDataOptsObject = {
     schema : {
         response : {
-            200 : {
-                type :  'object',
-                properties : {
-                    // id : {type : 'string'},
-                    name : {type : 'string'}
-                }
-            }
+            200 : Item
         }
+    },
+    handler : function (req, res) {
+        const {id} = req.params
+        const item = data.find((ele) => ele.id == id)
+        res.send(item)
     }
 }
 function userRoute (fastify, options, done){
 
-    fastify.get('/data', getDataOpts, (req, res) => {
-        res.send( data)
-    })
+    // fastify.get('/data', getDataOpts, (req, res) => {
+    //     res.send( data)
+    // })
 
-    fastify.get('/filterData/:id', getDataOptsObject, (req, res) => {
-        const {id} = req.params
-        const item = data.find((ele) => ele.id == id)
-        res.send(item)
-    })
+    // fastify.get('/filterData/:id', getDataOptsObject, (req, res) => {
+    //     const {id} = req.params
+    //     const item = data.find((ele) => ele.id == id)
+    //     res.send(item)
+    // })
+    //minimize code add handler in schema
+    
+    fastify.get('/data', getDataOpts)
+
+    fastify.get('/filterData/:id', getDataOptsObject )
+
     done()
 }
 module.exports = userRoute
