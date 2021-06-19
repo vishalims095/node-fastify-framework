4922e0bb0bf15c3fc1b43254217336af95724800
vishal sharma <vishal.sharma@fluper.in>
2021-06-19 09:34:39 +0530

    routing

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [{
+    id : '1',
+    name : "vishal"
+},
+{
+    id : '2',
+    name : "suraj"
+},
+{
+    id : '3',
+    name : "test"
+}
+]
+module.exports = data
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const fastify = require('fastify')({logger : true})
+const data = require('./data')
 port = 3000
 
 fastify.get('/testApi', (req, res) => {
     res.send({test : "It's working"})
+})
+
+fastify.get('/filterData/:id', (req, res) => {
+    const {id} = req.params
+    const item = data.find((ele) => ele.id == id)
+    res.send(item)
 })
 
 const start = async() =>{
