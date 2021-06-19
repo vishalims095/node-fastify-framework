f567aa1bd49f46a938269de46d7de4bb4ddf69cf
Vishal Sharma <vishal.sharma1@mail.vinove.com>
2021-06-19 10:28:25 +0530

    fastify schema

--- routes/items.js
+++ routes/items.js
@@ -1,7 +1,23 @@
 const data = require('../data')
+const getDataOpts = {
+    schema : {
+        response : {
+            200 : {
+                type :  'array',
+                items : {
+                    type : 'object',
+                    properties : {
+                        id : {type : 'number'},
+                        name : {type : 'string'}
+                    }
+                }
+            }
+        }
+    }
+}
 function userRoute (fastify, options, done){
-    fastify.get('/testApi', (req, res) => {
-        res.send({test : "It's working"})
+    fastify.get('/data', getDataOpts, (req, res) => {
+        res.send( data)
     })
 
     fastify.get('/filterData/:id', (req, res) => {
